3e53f4651c4166ddccbd07686d27ecb13380fe5e
Artem Goncharov <Artem.goncharov@gmail.com>
2021-06-17 10:19:16 +0200

    fix linters on the copy-zuul-to-quay

--- tools/copy-zuul-to-quay.sh
+++ tools/copy-zuul-to-quay.sh
@@ -4,6 +4,6 @@ TAG=$1
 
 
 for img in zuul-executor zuul-scheduler zuul-merger zuul-web nodepool-launcher nodepool-builder; do
-  echo -n "Copying docker.io/zuul/${img}:$TAG to quay.io/opentelekomcloud/${img}:$TAG"
-  skopeo copy docker://docker.io/zuul/${img}:${TAG} docker://quay.io/opentelekomcloud/${img}:${TAG} -a
+    echo -n "Copying docker.io/zuul/${img}:$TAG to quay.io/opentelekomcloud/${img}:$TAG"
+    skopeo copy docker://docker.io/zuul/${img}:${TAG} docker://quay.io/opentelekomcloud/${img}:${TAG} -a
 done
